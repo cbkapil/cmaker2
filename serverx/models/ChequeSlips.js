5d53b5ba3064779ad93f01f6b9566ba4245fd2bd
cbkapil <kapilkumar@cognicsys.com>
2022-11-20 08:25:02 -0800

    first commit

---
+++ serverx/models/ChequeSlips.js
@@ -0,0 +1,23 @@
+import mongoose, { Schema, ObjectId } from "mongoose";
+
+const checkDataSchema = new mongoose.Schema(
+  {
+    firmname:{type:String},
+   
+    bank: { type: String },
+    branch: { type: String },
+    accountno: { type: Number },
+    partyname:{type:String},
+    chequeno:{type:mongoose.Schema.Types.Mixed},
+    amount:{type:Number},
+    chequedate:{type:Date},
+    firmid:{type:mongoose.Schema.Types.ObjectId, ref:"firmData"},
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: "userData" },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const Chequeslips = mongoose.model("chequeslips", checkDataSchema);
+export default Chequeslips;
---
+++ serverx/routes/CreateChequeSlip.js
@@ -0,0 +1,56 @@
+import Chequeslips from "../models/ChequeSlips.js";
+import UserData from "../models/UserData.js";
+import express from "express";
+import auth from "./Auth.js";
+const router = express.Router();
+
+router.post("/createchequeslip", auth, async (req, res) => {
+  console.log(req.userId);
+  const {
+   chequeFirmid,
+   chequefirmname,
+   formValues
+  } = req?.body;
+  
+
+  try {
+    const userPromises = formValues.map(user => {
+      return new Promise((resolve, reject) => {
+        const dataa = new Chequeslips({
+          firmname:chequefirmname,
+           bank:user.bankname ,
+          branch: user.branch,
+          accountno:user.accountno ,
+          partyname:user.customer,
+          chequeno:user.chequeno,
+          amount:user.amount,
+          chequedate:user.chequedate,
+          firmid:chequeFirmid,
+          userid:req.userId,
+        });
+        
+        dataa.save((error, result) => {
+          if (error) {
+            reject(error)
+          } else{
+          resolve(result)
+        console.log('kkkkkk',result)
+         UserData.findByIdAndUpdate(
+                    req.userId,
+                    { $push: { chequeslips: result._id } },
+                    function (err, result) {
+                    if (err) reject();
+                      resolve(result)
+                    }
+                  );};
+        })
+      })
+    });
+
+    res.status(201).json({ message: "Firm Data added", status: true });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+export default router;
